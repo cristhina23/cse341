966bf89346ecc05a1beabed5ade18fec66822466
Cristhina <viszellechacon@gmail.com>
2025-08-23 13:01:34 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,6 @@
-const { createServer } = require('nede:http');
+const express = require('express')
+const app = express()
 
-const hostname = '127.0.0.1';
-const port = 3000;
 
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
-  res.send('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
-
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
