3bcb0fd3fffc5cf426f45cfd45f03f9d85bfd13b
Cristhina <viszellechacon@gmail.com>
2025-09-27 18:19:49 -0500

    update

--- backend/swagger.js
+++ backend/swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: 'My API',
     description: 'My Contacts API documentation',
   },
-  host: 'localhost:3000',   
-  schemes: ['http'],        
+  host: 'cse341-stnx.onrender.com/',   
+  schemes: ['https'],        
 };
 
 const outputFile = './swagger.json';
